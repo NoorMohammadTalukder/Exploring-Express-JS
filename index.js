78b89b786040e50afa23876ff431985e2a34da27
TalukderNoor <119832596+TalukderNoor@users.noreply.github.com>
2023-05-04 22:57:53 +0600

    simple text response

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express=require('express');
+var express = require("express");
 
-app=express();
+app = express();
 
-app.get("/",function(req,res){
-    res.send("hello world!!!");
-})
-app.post("/contact",function(req,res){
-    res.send("this is contact!!!");
-})
-app.put("/about",function(req,res){
-    res.send("this is about!!!");
-})
-app.delete("/terms",function(req,res){
-    res.send("this terms!!!");
+//-------routing
+app.get("/", function (req, res) {
+  res.send("hello world!!!");
+});
+app.post("/contact", function (req, res) {
+  res.send("this is contact!!!");
+});
+app.put("/about", function (req, res) {
+  res.send("this is about!!!");
+});
+app.delete("/terms", function (req, res) {
+  res.send("this terms!!!");
+});
+
+//-----simple string response
+
+app.get("/one",function(req,res){
+    res.send("this is simple text response");
+});
+
+app.post("/two",function(req,res){
+    res.end("this is simple text response");
 })
 
-app.listen(8000,function(){
-    console.log("server running on port 8000");
-})
+app.listen(8000, function () {
+  console.log("server running on port 8000");
+});
