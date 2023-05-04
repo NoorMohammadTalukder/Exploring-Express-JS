8ecba1b022ce00cd36547de0730d577d80d22e86
TalukderNoor <119832596+TalukderNoor@users.noreply.github.com>
2023-05-04 19:04:54 +0600

    routing

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app=express();
 app.get("/",function(req,res){
     res.send("hello world!!!");
 })
+app.post("/contact",function(req,res){
+    res.send("this is contact!!!");
+})
+app.put("/about",function(req,res){
+    res.send("this is about!!!");
+})
+app.delete("/terms",function(req,res){
+    res.send("this terms!!!");
+})
 
 app.listen(8000,function(){
     console.log("server running on port 8000");
